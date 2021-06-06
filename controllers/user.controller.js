01716f5d3958a187eff3d5545664042bb6c2d311
Sourav <people.outofbox@gmail.com>
2021-06-06 14:41:32 +0530

    user registration & login authorization

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bryptjs = require("bcryptjs");
 
 const getusers = (req, res) => {
     User.find()
@@ -8,12 +9,39 @@ const getusers = (req, res) => {
 const addUser = (req, res) => {
     console.log(req.body)
     const newUser = new User({...req.body});
-    newUser.save()
-    .then(user => res.send(user))
-    .catch(err => res.send(err));
+    bryptjs.genSalt(10, (err, salt) => {
+        bryptjs.hash(newUser.password, salt, (err, hash) => {
+            newUser.password = hash;
+            newUser.save()
+            .then(user => {return res.redirect("/login")})
+            .catch(err => res.send(err));
+        })
+    })
+    
 };
 const login = (req, res) => {
     res.render("login")
+}
+
+const checkLogin = (req, res) => {
+    const userObj = req.body;
+    console.log("1");
+    User.findOne({email: userObj.email}).then(user => {
+        console.log("2");
+        bryptjs.compare(userObj.password, user.password, (err, isMatch) => {
+            if(isMatch) {
+                return res.redirect("/dashboard");
+            } else {
+                console.log("4");
+                return res.redirect("/login");
+            }
+        })
+    })
+    .catch(err => {
+        console.log("3");
+        return res.redirect("/login");
+    })
+
 }
 const register = (req, res) => {
     res.render("register")
@@ -22,5 +50,6 @@ module.exports = {
     getusers,
     addUser,
     login,
-    register
+    register,
+    checkLogin
 }
--- routes/index.route.js
+++ routes/index.route.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.get("/",userController.login);
+router.get("/dashboard",(req, res) => {
+    res.render("dashboard")
+});
+
 router.get("/login",userController.login);
+router.post("/login",userController.checkLogin);
+
 router.get("/register",userController.register);
 router.get("/users",userController.getusers);
 router.post("/user",userController.addUser)
